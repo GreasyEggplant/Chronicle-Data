2aca7388f296fa6555bb5b016212b84cd735194c
Bill Belanger <wj.belanger@gmail.com>
2016-03-25 14:44:17 -0400

    Figured out most Family types

--- entities/enums/Family.cs
+++ entities/enums/Family.cs
@@ -8,7 +8,29 @@ namespace com.greasyeggplant.chronicle.data.entities
 {
     public enum Family
     {
-        Unknown = -1,
-
+        Unspecified = -1,
+        None = 0,
+        Beast = 1,
+        Demon = 2,
+        Dragon = 3,
+        Location = 4,
+        Equipment = 5,
+        UnknownType6 = 6,
+        Giant = 7,
+        Goblin = 8,
+        Kalphite = 9,
+        Ogre = 10,
+        Orc = 11,
+        Pirate = 12,
+        Potion = 13,
+        Ship = 14,
+        UnknownType16 = 16,
+        Troll = 17,
+        TzHaar = 18,
+        Undead = 19,
+        Ally = 20,
+        UnknownType21 = 21,
+        Vampyre = 22,
+        UnknownType23 = 23
     }
 }
